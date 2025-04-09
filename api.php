407aa6d6bcede21abf3333b8f71001b01a2a0a71
Felimon Abogado <felimonabogado@gmail.com>
2025-04-09 19:03:03 +0800

    Additional headers to prevent CORS error

--- api.php
+++ api.php
@@ -1,5 +1,14 @@
 <?php
+header("Access-Control-Allow-Origin: *"); // Allow all origins
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS"); // Allow specific methods
+header("Access-Control-Allow-Headers: Content-Type"); // Allow specific headers
 header("Content-Type: application/json");
+
+if ($_SERVER["REQUEST_METHOD"] == "OPTIONS") {
+    http_response_code(200);
+    exit();
+}
+
 include "db.php";
 
 $method = $_SERVER["REQUEST_METHOD"];
